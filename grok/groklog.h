e564d8b9e1bbc5d7c6794cb24b53eb9238d7e714
cbookshu <cbookshu@outlook.com>
2023-02-09 14:36:02 +0800

    默认拉下来的时候，跑release没有日志，先把日志放开

--- grok/groklog.h
+++ grok/groklog.h
@@ -5,5 +5,7 @@
 #define DBG(fmt,...)        \
     printf("[%s][%d]:" fmt "\n",__FILE__,__LINE__,##__VA_ARGS__);
 #else
-#define DBG(fmt,...)        
+// TODO: 未来把DBG改为真正的日志代码
+#define DBG(fmt,...)        \
+    printf("[%s][%d]:" fmt "\n",__FILE__,__LINE__,##__VA_ARGS__);
 #endif
